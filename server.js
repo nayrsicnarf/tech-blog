f4720d4baafa2a78e1dccb226247259c1f3b60cf
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-06-01 23:05:39 -0700

    add updates

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
